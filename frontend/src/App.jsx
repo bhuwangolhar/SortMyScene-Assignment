ccde9aa4242f7957dd14f75772a7122eddd2b33c
bhuwangolhar <bhu1golhar@gmail.com>
2026-06-19 17:51:31 +0530

    Add necessary scripts and packages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,7 @@
+import AppRoutes from "./routes/AppRoutes";
+
 function App() {
-    return (
-        <div>
-            <h1>SortMyScene Event Booking System</h1>
-        </div>
-    );
+    return <AppRoutes />;
 }
 
 export default App;
